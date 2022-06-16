37fcb8f08292414584609d5896ee585f628e1e13
Obinna <obi.udeogu@gmail.com>
2022-06-15 19:24:22 -0600

    Initial commit Completed all TODOs

--- backend/src/api.py
+++ backend/src/api.py
@@ -17,7 +17,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -25,9 +25,26 @@ CORS(app)
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks}
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks', methods=["GET"])
+def retrieve_drinks():
+    selection = Drink.query.order_by(Drink.id).all()
+    drinks = [drink.short() for drink in selection]
+
+    if len(drinks) == 0:
+        abort(404)
+
+    return jsonify(
+        {
+            "success": True,
+            "drinks": drinks
+        }
+    )
 
 
 '''
@@ -35,9 +52,27 @@ CORS(app)
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks}
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def retrieve_drinks_detail(payload):
+    selection = Drink.query.order_by(Drink.id).all()
+    drinks = [drink.long() for drink in selection]
+
+    if len(drinks) == 0:
+        abort(404)
+
+    return jsonify(
+        {
+            "success": True,
+            "drinks": drinks
+        }
+    )
 
 
 '''
@@ -46,9 +81,33 @@ CORS(app)
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+    where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks', methods=["POST"])
+@requires_auth('post:drinks')
+def create_drink(payload):
+
+    try:
+        body = request.get_json()
+
+        new_title = body.get("title", None)
+        new_recipes = body.get("recipe", None)
+
+        drink = Drink(title=new_title, recipe=json.dumps(new_recipes))
+        drink.insert()
+
+        return jsonify(
+            {
+                "success": True,
+                "drinks": [drink.long()]
+            }
+        )
+    except BaseException:
+        abort(422)
 
 
 '''
@@ -59,9 +118,38 @@ CORS(app)
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+    where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/<int:id>', methods=["PATCH"])
+@requires_auth('patch:drinks')
+def update_drink(payload, id):
+    try:
+        body = request.get_json()
+
+        new_title = body.get("title", None)
+        new_recipes = body.get("recipe", None)
+
+        drink = Drink.query.get(id)
+        if new_title is not None:
+            drink.title = new_title
+
+        if new_recipes is not None:
+            drink.recipe = json.dumps(new_recipes)
+
+        drink.update()
+
+        return jsonify(
+            {
+                "success": True,
+                "drinks": [drink.long()]
+            }
+        )
+    except BaseException:
+        abort(404)
 
 
 '''
@@ -71,9 +159,27 @@ CORS(app)
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    returns status code 200 and json {"success": True, "delete": id}
+    where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/<int:id>', methods=["DELETE"])
+@requires_auth('patch:drinks')
+def delete_drink(payload, id):
+    try:
+        drink = Drink.query.get(id)
+        drink.delete()
+
+        return jsonify(
+            {
+                "success": True,
+                "delete": id
+            }
+        )
+    except BaseException:
+        abort(404)
 
 
 # Error Handling
@@ -108,7 +214,22 @@ def unprocessable(error):
 '''
 
 
+@app.errorhandler(404)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
+
+
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    return response, ex.status_code
